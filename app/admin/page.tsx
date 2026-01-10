ef9aab1387d17ffb98b89454220f4f7944286c2b
Gerald Gyeabour <geraldsix89@gmail.com>
2026-01-10 08:58:57 +0000

    admin commented

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
-export default function AdminIndex() {
-  redirect("/admin/leads");
-}
+// export default function AdminIndex() {
+//   redirect("/admin/leads");
+// }
